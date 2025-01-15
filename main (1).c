b42749430cded549b322ff3e1af64d7ab93f66ab
Vinita shah <124777180+vinita2003@users.noreply.github.com>
2025-01-15 22:51:41 +0530

    Update main (1).c

--- main (1).c
+++ main (1).c
@@ -1,52 +1,26 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
-
 int main()
 {
-    // printf("Hello World");
-    
     int row;
     int column;
-    
     scanf("%d", &row);
     scanf("%d", &column);
-    
-    int array[row][column];
-    
-    
-    for(int i = 0; i < row; i++){
+     int array[row][column];
+     for(int i = 0; i < row; i++){
         for(int j =0; j < column; j++){
-            scanf("%d", &array[i][j]);
-        }
+            scanf("%d", &array[i][j]);}
     }
-    
     int transpose[row][column];
-    
     for(int i = 0; i < row; i++){
         for(int j =0; j < column; j++){
-            // scanf("%d", &array[i][j]);
-            
-            transpose[i][j] = array[j][i];
+          transpose[i][j] = array[j][i];
         }
     }
-    
     for(int i = 0; i < row; i++){
         for(int j =0; j < column; j++){
-            // scanf("%d", &array[i][j]);
-            
             printf("%d ", transpose[i][j]);
         }
         printf("\n");
     }
-    
-    
-
     return 0;
 }
