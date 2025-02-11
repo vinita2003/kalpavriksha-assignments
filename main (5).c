6f91aa7f4cb0cd7e81669a29193166e426afb909
Vinita shah <124777180+vinita2003@users.noreply.github.com>
2025-02-11 20:56:37 +0530

    Update main (5).c

--- main (5).c
+++ main (5).c
@@ -1,53 +1,35 @@
 #include<stdio.h>
 #include<string.h>
-
 int main(){
     char name[100];
     scanf("%[^\n]%*c", name);
-    // printf("%s", name);
-
 if(strlen(name) == 0){
     return 0;
 }
-    
     char ans[100];
     int ansindex = 0;
-
-    
-
-  char ch;
+    char ch;
     for(int i = 0; i < strlen(name); i++){
-        
         if(i == 0){
             ans[ansindex++] = name[i];
             ch = name[i];
-           
-        }else{
-           
-            if(ch == name[i] || name[i-1] == name[i]){
+            }else{
+           if(ch == name[i] || name[i-1] == name[i]){
                 ans[ansindex] = '1';
-               
                 ch = ans[ansindex++];
-                // printf("%c - caserd", ch);
-                
-            }else if(ch == '1'){
+                }else if(ch == '1'){
                     ans[ansindex++] = name[i];
-                    // printf("%c", name[i]);
                          ch = name[i];
             }else{
-                // printf("%c", ch);
                    ans[ansindex++] = ch;
              } }
     }
-  
-         
-        for(int i = 0; i < ansindex; i++){
-         if(ans[i] == '1'){
+  for(int index = 0; index < ansindex; index++){
+         if(ans[index] == '1'){
             printf("-"); 
          }
-        printf("%c", ans[i]);
+        printf("%c", ans[index]);
      }
-     
-    return 0;
+     return 0;
 }
               
